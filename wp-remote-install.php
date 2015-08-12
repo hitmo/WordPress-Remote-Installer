2046f68fbf51c8ce0cb8cb9338d61112afac568e
hitmo <hitmota21@gmail.com>
2015-08-12 18:39:35 +0530

    Update wp-remote-install.php

--- wp-remote-install.php
+++ wp-remote-install.php
@@ -3,7 +3,8 @@
 // Global Configuration
 set_time_limit( 0 );
 error_reporting( E_ALL );
-
+ini_set("max_execution_time", "3000");
+ini_set("display_error",1);
 // GitHub Information
 define( 'GITHUB_USERNAME' , 'lucanos' );
 define( 'GITHUB_PROJECT'  , 'WordPress-Remote-Installer' );
